dc1d9e8c984ba74a1ee6d00d6d8a98e64bd74d14
NakanoMiku13 <danulricht@hotmail.com>
2021-07-29 20:03:32 -0500

    Update Documentacion

--- SafeBox/SafeBox.ino
+++ SafeBox/SafeBox.ino
@@ -7,6 +7,7 @@ const byte ROWS=4;
 const byte COLS=4;
 const byte LcdRow=2;
 const byte LcdCol=16;
+//Contraseña por defecto
 const char DfPwd[]={'1','4','0','8','0','2','A'};
 const byte rowPin[ROWS]={9,8,7,6};
 const byte colPin[COLS]={5,4,3,2};
@@ -22,6 +23,7 @@ Servo servo;
 char text[17];
 char tmp[17];
 int i=0,iT=0;
+//Funcion para checar si hay algo guardado en la memoria, en este caso la contraseña ingresada por el usuario
 auto Check()->void{
   int x=EEPROM.read(0);
   i=0;
@@ -31,16 +33,20 @@ auto Check()->void{
     }
   }
 }
+//Función para ajustar la fila en la que se imprimirá el mensaje
 auto Set(int x)->void{
   lcd.setCursor(0,x);
 }
+//Obtener la tecla pulsada por el usuario en el keypad
 auto GetKey()->char{
   char customKey=customKeypad.getKey();
   return customKey;
 }
+//Limpiar pantalla
 auto Clean()->void{
   lcd.clear();
 }
+//Funcion donde vemos el ingreso de la nueva contraseña
 auto PrintText()->void{
   Set(0);
   lcd.print("Ingresalo:");
@@ -48,6 +54,9 @@ auto PrintText()->void{
   if(customKey){
     Clean();
     if(customKey=='*'){
+      //En la posicion 0 de la memoria de la arduino se guarda la cantidad de datos a almacenar
+      //EEPROM nos permite guardarla como un arreglo EEPROM.write(posicion,valor)
+      //Solo un valor por celda y la memoria es limitada, cuidado
       EEPROM.write(0,i);
       Clean();
       Set(0);
@@ -70,6 +79,7 @@ auto PrintText()->void{
   }
   PrintText();
 }
+//Funcion General para nueva contraseña
 auto NewPwd()->void{
   i=0;
   Clean();
@@ -79,6 +89,7 @@ auto NewPwd()->void{
   lcd.print("Ingresalo: ");
   PrintText();
 }
+//Funcion para imprimir contraseña, en caso de que no se haya cambiado previamente, se imprimirá la default
 auto PrintPwd()->void{
   Clean();
   Set(0);
@@ -92,6 +103,7 @@ auto PrintPwd()->void{
   delay(2500);
   Clean();
 }
+//Funcion de configuración de contraseña, ver o cambiar contraseña
 auto SetConfig()->void{
    Set(0);
    lcd.print("1)Cambiar pwd");
@@ -114,6 +126,7 @@ auto SetConfig()->void{
     SetConfig();
    }
 }
+//Funcion para checar que la contraseña ingresada por el usuario sea igual a la guardada
 auto PwdCheck()->bool{
   for(int j=0;j<i;j++){
     if(tmp[j]!=text[j]){
@@ -122,16 +135,20 @@ auto PwdCheck()->bool{
   }
   return true;
 }
+//Limpiar el arreglo temporal de la contraseña
 auto ClearTmp()->void{
   memset(tmp,0,sizeof(tmp));
   iT=0;
 }
+//Funcion para abrir el servo
 auto Open()->void{
   servo.write(65);
 }
+//Funcion para cerrar el servo
 auto Close()->void{
   servo.write(5);
 }
+//Funcion donde se ingresa la contraseña y posteriormente es verificada para ver si es la correcta
 auto TypePwd()->void{
   Set(0);
   lcd.print("Contraseia:");
@@ -164,6 +181,9 @@ auto TypePwd()->void{
   }
   TypePwd();
 }
+//Menu de opciones, al usuario no se le despliega, pero ingresando D se abre la configuración para cambiar o ver la contraseña
+//Y la opción * cierra el servo, ya que despues de ingresar la contraseña se queda abierto (hasta que se apague la arduino)
+//Y cualquier otra telca nos permite ingresar la contraseña
 auto Menu()->void{
   char x=GetKey();
   if(x){
